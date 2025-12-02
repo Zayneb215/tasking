e97855b6e16f2bf81139bf69cc905336cee6077e
Zayneb Bellil <zainebbellil14@gmail.com>
2025-12-02 22:06:54 +0100

    add to do list component

---
+++ frontend/src/app/components/content-area-components/todo-list/todo-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TodoService, Todo } from '../../../services/todo.service';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+  newTodo = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.todoService.getMyTodos().subscribe(t => this.todos = t);
+  }
+
+  addTodo() {
+    if (!this.newTodo.trim()) return;
+
+    this.todoService.createTodo(this.newTodo).subscribe(() => {
+      this.newTodo = '';
+      this.loadTodos();
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todoService.deleteTodo(id).subscribe(() => {
+      this.loadTodos();
+    });
+  }
+}
--- frontend/src/app/components/content-area/content-area.component.ts
+++ frontend/src/app/components/content-area/content-area.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { UserInfoComponent } from '../content-area-components/user-info/user-info.component';
+import { TodoListComponent } from '../content-area-components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-content-area',
-  imports: [UserInfoComponent],
+  imports: [TodoListComponent],
   templateUrl: './content-area.component.html',
   styleUrl: './content-area.component.scss'
 })
