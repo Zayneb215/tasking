82ad4aaab93f5c6078a172f98926426d5a614dea
Zayneb Bellil <zainebbellil14@gmail.com>
2025-12-02 15:28:30 +0100

    user auth service in register component and use user card  to home page

--- frontend/src/app/components/register-form/register-form.component.ts
+++ frontend/src/app/components/register-form/register-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
@@ -15,22 +16,26 @@ export class RegisterFormComponent implements OnInit {
 
   constructor(
     private readonly fb: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router      // <-- Inject Router here
   ) {}
 
-  ngOnInit() {   // <-- MUST run, otherwise validators never initialize
+  ngOnInit() {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
+
   submit() {
     if (this.form.valid) {
       this.auth.register(this.form.value).subscribe({
-        next: (res) => {
-          console.log("User registered!", res);
+        next: () => {
           alert('Registration successful!');
+
+          // -------------- REDIRECT --------------
+          this.router.navigate(['/connexion']);   // <-- redirect to login page
         },
         error: (err) => {
           alert('Registration failed: ' + (err.error?.message || 'Unknown error'));
--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserCardComponent, UserProfile } from '../../components/user-card/user-card.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,
+  imports: [UserCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  user! : UserProfile
 
+  constructor(private auth: AuthService) {}
+  
+  ngOnInit(): void {
+    this.user = this.auth.getProfile();
+  }
 }
